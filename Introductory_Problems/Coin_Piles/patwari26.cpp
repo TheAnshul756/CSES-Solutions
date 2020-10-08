8781c49aa66c1addab6ae9c11198c51229453c9d
patwari26 <patwari.ayush26@gmail.com>
2020-10-09 02:42:50 +0530

    added two sets

--- Introductory_Problems/Coin_Piles/patwari26.cpp
+++ Introductory_Problems/Coin_Piles/patwari26.cpp
@@ -8,6 +8,9 @@ int main()
     while (t--)
     {
         cin >> a >> b;
+        // we have to remove 3 coins in every step so (a+b)%3 should be zero
+        // the pile with more coins should not contain greater coins than double of first pile
+        // else it can never be removed
         cout << ((a + b) % 3 == 0 && (min(a, b) * 2 >= max(a, b)) ? "YES" : "NO") << endl;
     }
 }
