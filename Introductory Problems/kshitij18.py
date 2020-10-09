9d8c4530f9f96b9fb7cd15c249a3b8f56c1d58d1
Kshitij <kshitijnath@gmail.com>
2020-10-09 11:13:10 +0530

    Conformed to the guidlines of contrib.md

--- Introductory Problems/kshitij18.py
+++ Introductory Problems/kshitij18.py
@@ -1,12 +1,18 @@
 # recursive function to solve the problem
 def solve(number):
+    # base condition when the number hits 1
     if number == 1:
         print(number, end=' ')
         return
 
+    # printing the current number
     print(number, end=' ')
+
+    # if the number is even
     if(number % 2 == 0):
         solve(number//2)
+
+    # if the number is odd
     elif(number % 2 != 0):
         solve(number*3+1)
 
